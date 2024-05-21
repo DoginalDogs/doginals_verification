9053bcbfddbd93ab8c601511ba2d5f092a03be32
ml350 <ebu.pro@gmail.com>
2024-05-21 18:51:03 +0200

    test

--- static/main.js
+++ static/main.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     let userId = new URLSearchParams(window.location.search).get('user_id');
-    console.log(userId)
     let clientId = '1195885802786394154';
-    let redirectUri = 'https://doginal-dogs-verification-2cc9b2edc81a.herokuapp.com/callback'
+    let redirectUri = 'https://doginal-dogs-verification-2cc9b2edc81a.herokuapp.com/callback';
     let oauthSuccess = new URLSearchParams(window.location.search).get('oauth_success') === 'True';
 
     const loginDiscordButton = document.getElementById('loginDiscord');
-    const loginWalletButton = document.getElementById('loginWallet'); 
+    const loginWalletButton = document.getElementById('loginWallet');
     const logoutDiscordButton = document.getElementById('logoutDiscord');
     
     if (logoutDiscordButton) {
-        logoutDiscordButton.addEventListener('click', function() {
-            logoutFromDiscord();
-        });
+        logoutDiscordButton.addEventListener('click', logoutFromDiscord);
     }
 
     if (loginDiscordButton) {
@@ -29,12 +26,11 @@ document.addEventListener('DOMContentLoaded', function() {
     if (oauthSuccess) {
         loginDiscordButton.style.display = 'none';
         logoutDiscordButton.style.display = 'block';
-        loginWalletButton.style.display = 'block'; 
+        loginWalletButton.style.display = 'block';
     } 
 
     if (loginWalletButton) {
         loginWalletButton.addEventListener('click', function() {
-            // Call signMessageWithDogeLabsWallet function
             if (typeof window.dogeLabs !== 'undefined') {
                 signMessageWithDogeLabsWallet(userId);
             } else {
@@ -44,87 +40,72 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 }); 
 
-function logoutFromDiscord() { 
+function logoutFromDiscord() {
     sessionStorage.removeItem('discordToken');
-    // or localStorage.removeItem('discordToken');
-
-    // Update UI to reflect logged out state
     document.getElementById('loginDiscord').style.display = 'block';
     document.getElementById('loginWallet').style.display = 'none';
-    document.getElementById('logoutDiscord').style.display = 'none'; 
+    document.getElementById('logoutDiscord').style.display = 'none';
     alert('You have been logged out from Discord.');
 }
 
 async function getDoginals(userId, cursor = 0, allInscriptions = []) {
-    window.dogeLabs.getInscriptions(cursor)
-        .then(response => {
-            console.log("Inscriptions:", response);
+    try {
+        const response = await window.dogeLabs.getInscriptions(cursor);
+        console.log("Inscriptions:", response);
 
-            if (response.list.length === 0 && cursor === 0) {
-                // No inscriptions found, show the updated alert message
-                alert('Sorry, you have no Doginal Dogs in your wallet! Adopt a dog, come back, and try again!');
-                return;
-            }
+        if (response.list.length === 0 && cursor === 0) {
+            alert('Sorry, you have no Doginal Dogs in your wallet! Adopt a dog, come back, and try again!');
+            return;
+        }
 
-            // Concatenate the new inscriptions with the previous array
-            allInscriptions = allInscriptions.concat(response.list);
+        allInscriptions = allInscriptions.concat(response.list);
 
-            // Check if there are more inscriptions to load
-            if (response.list.length === 20) {
-                // Recursively call getDoginals with the new cursor value
-                getDoginals(userId, cursor + 20, allInscriptions);
-            } else {
-                // All inscriptions have been loaded, proceed with the logic
-                const inscriptionData = allInscriptions.map(inscription => ({
-                    user_id: userId,
-                    inscriptionId: inscription.inscriptionId,
-                    inscriptionNumber: inscription.inscriptionNumber,
-                    address: inscription.address
-                }));
+        if (response.list.length === 20) {
+            await getDoginals(userId, cursor + 20, allInscriptions);
+        } else {
+            const inscriptionData = allInscriptions.map(inscription => ({
+                user_id: userId,
+                inscriptionId: inscription.inscriptionId,
+                inscriptionNumber: inscription.inscriptionNumber,
+                address: inscription.address
+            }));
 
-                // Send the extracted data back to your server
-                fetch('https://doginal-dogs-verification-2cc9b2edc81a.herokuapp.com/verify_holder', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ optimized_data: inscriptionData, user_id: userId })
-                })
-                .then(response => response.json())
-                .then(data => {  
-                    console.log(data);
-                    if (data && data.length > 0) {
-                        // Check for both message and error properties
-                        let alertShown = false;
-                        let firstError = "";
+            const res = await fetch('https://doginal-dogs-verification-2cc9b2edc81a.herokuapp.com/verify_holder', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ optimized_data: inscriptionData, user_id: userId })
+            });
+            
+            const data = await res.json();
+            console.log(data);
 
-                        data.forEach(response => {
-                            if (!alertShown) {
-                                if (response.hasOwnProperty('message')) {
-                                    alert(response.message);
-                                    alertShown = true; // Set the flag after showing the alert
-                                } else if (response.hasOwnProperty('error') && !firstError) {
-                                    firstError = response.error; // Store the first error encountered
-                                }
-                            }
-                        });
+            if (data && data.length > 0) {
+                let alertShown = false;
+                let firstError = "";
 
-                        // If no message was found and an error exists, show the first error
-                        if (!alertShown && firstError) {
-                            alert(firstError);
+                data.forEach(response => {
+                    if (!alertShown) {
+                        if (response.hasOwnProperty('message')) {
+                            alert(response.message);
+                            alertShown = true;
+                        } else if (response.hasOwnProperty('error') && !firstError) {
+                            firstError = response.error;
                         }
-                    } else {
-                        alert('Unexpected response format from server.');
                     }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
                 });
+
+                if (!alertShown && firstError) {
+                    alert(firstError);
+                }
+            } else {
+                alert('Unexpected response format from server.');
             }
-        })
-        .catch(error => {
-            console.error("Error fetching inscriptions:", error.message);
-        }); 
+        }
+    } catch (error) {
+        console.error("Error fetching inscriptions:", error.message);
+    }
 }
 
 async function signMessageWithDogeLabsWallet(userId) {
@@ -135,12 +116,12 @@ async function signMessageWithDogeLabsWallet(userId) {
 
     try {
         const accounts = await window.dogeLabs.requestAccounts();
- 
+
         if (!accounts || accounts.length === 0) {
             alert("No accounts found. Please ensure your wallet is connected and try again.");
             return;
         }
-        
+
         const messageToSign = "Sign to Prove Ownership";
         const messageType = "text";
 
